8162255d069610f061d9844315765d4d73f5540b
marwasoumia <marwa.blaugrana@gmail.com>
2019-01-13 15:31:36 +0100

    commit pull

--- src/main/java/service/MatrixMathematics.java
+++ src/main/java/service/MatrixMathematics.java
@@ -10,6 +10,16 @@ public class MatrixMathematics {
 	 */
 	private MatrixMathematics() {
 	}
+   
+   public static Matrix transpose(Matrix matrix) {
+		Matrix transposedMatrix = new Matrix(matrix.getNcols(), matrix.getNrows());
+		for (int i=0;i<matrix.getNrows();i++) {
+			for (int j=0;j<matrix.getNcols();j++) {
+				transposedMatrix.setValueAt(j, i, matrix.getValueAt(i, j));
+			}
+		}
+		return transposedMatrix;
+	}
 
 
 	/**
